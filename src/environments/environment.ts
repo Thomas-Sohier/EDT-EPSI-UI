1de93fe489aba8e5c8b08722f5d6fe09de6d0e43
Thomas <thomas.sohier44@gmail.com>
2019-09-23 09:00:15 +0200

    correction local API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const API_URL = 'https://api.thomas-sohier.fr:8443/cour';
+export const API_URL = 'https://api.thomas-sohier.fr:8443';
 
 /*
  * For easier debugging in development mode, you can import the following file
